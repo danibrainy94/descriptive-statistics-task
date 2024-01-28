43b61714a6a4aee221f1d76930b0ec5a53f2bd53
danibrainy94 <danibrainy94@gmail.com>
2024-01-28 20:30:11 +0100

    main

--- script.js
+++ script.js
@@ -173,6 +173,18 @@ class MovieStore {
         this.rentedMovies = []
     }
 
+    rentMovie(title) {
+        const movieIndex = this.movies.findIndex(movie => movie.title === title)
+
+        if (movieIndex !== -1 && this.movies[movieIndex].availableCopies > 0) {
+        this.movies[movieIndex].availableCopies--
+        this.rentedMovies.push(this.movies[movieIndex])
+        return `${title} rented successfully.`
+        } else {
+        return `Sorry, ${title} is not available for rent or no copies left.`
+        }
+    }
+
     
 }
 
